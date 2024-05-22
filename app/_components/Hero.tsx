531b1150f0f6d73752e58b39c37ce07233da0e24
Rizky Ramadhan <ramarizdev@gmail.com>
2024-05-22 16:19:56 +0700

    feat(hero): change positioning on mobile

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -22,21 +22,44 @@ const Hero = () => {
   }, [isPaused]);
 
   return (
-    <div className='relative aspect-square md:aspect-video object-cover'>
-      <video
-        ref={videoRef}
-        autoPlay
-        muted
-        loop
-        playsInline
-        className='h-full w-full object-cover'>
-        <source src='/assets/hero.mp4' type='video/mp4' />
-      </video>
-      <div className='absolute bottom-0 left-0 w-full py-20 bg-gradient-to-t from-black/50 to-transparent hidden lg:flex flex-col items-center gap-6'>
-        <h1 className='text-white text-2xl font-semibold'>
+    <div className='relative'>
+      <div className='relative aspect-square md:aspect-video object-cover overflow-hidden'>
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          loop
+          playsInline
+          className='h-full w-full object-cover'>
+          <source src='/assets/hero.mp4' type='video/mp4' />
+        </video>
+
+        <button
+          onClick={() => setIsMuted((prev) => !prev)}
+          className='absolute bottom-4 left-4 size-10 bg-black rounded-full flex items-center justify-center'>
+          <VolumeX
+            className={cn('size-5 text-white', isMuted ? 'block' : 'hidden')}
+          />
+          <Volume2
+            className={cn('size-5 text-white', isMuted ? 'hidden' : 'block')}
+          />
+        </button>
+        <button
+          onClick={() => setIsPaused((prev) => !prev)}
+          className='absolute bottom-4 right-4 size-10 bg-black rounded-full flex items-center justify-center'>
+          <Play
+            className={cn('size-5 text-white', isPaused ? 'block' : 'hidden')}
+          />
+          <Pause
+            className={cn('size-5 text-white', isPaused ? 'hidden' : 'block')}
+          />
+        </button>
+      </div>
+      <div className='md:absolute bottom-0 left-0 w-full p-4 md:py-20 md:bg-gradient-to-t from-black/50 to-transparent flex flex-col items-center gap-4 md:gap-6'>
+        <h1 className='text-black md:text-white text-2xl font-semibold'>
           With Love, Since 1837
         </h1>
-        <p className='text-white max-w-md text-center text-sm'>
+        <p className='text-black md:text-white md:max-w-md text-center text-sm'>
           An homage to an iconic motif from 1975 inspired by New York, T by
           Tiffany is an expression of love&apos;s endless potential.
         </p>
@@ -44,26 +67,6 @@ const Hero = () => {
           Shop Now
         </button>
       </div>
-      <button
-        onClick={() => setIsMuted((prev) => !prev)}
-        className='absolute bottom-4 left-4 size-10 bg-black rounded-full flex items-center justify-center'>
-        <VolumeX
-          className={cn('size-5 text-white', isMuted ? 'block' : 'hidden')}
-        />
-        <Volume2
-          className={cn('size-5 text-white', isMuted ? 'hidden' : 'block')}
-        />
-      </button>
-      <button
-        onClick={() => setIsPaused((prev) => !prev)}
-        className='absolute bottom-4 right-4 size-10 bg-black rounded-full flex items-center justify-center'>
-        <Play
-          className={cn('size-5 text-white', isPaused ? 'block' : 'hidden')}
-        />
-        <Pause
-          className={cn('size-5 text-white', isPaused ? 'hidden' : 'block')}
-        />
-      </button>
     </div>
   );
 };
